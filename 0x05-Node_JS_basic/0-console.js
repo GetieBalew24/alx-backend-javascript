25c4635a8d2ca725724d0e0418a02fbe1a5d7b53
GetieBalew24 <getiebalew2020@gmail.com>
2023-05-16 16:04:51 -0400

    add necessary package

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Getie Balew <https://github.com/GetieBalew24>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
