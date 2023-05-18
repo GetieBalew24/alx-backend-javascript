a5fa46e447734d76a0b8c7eab47d7b6c7e64511d
GetieBalew24 <getiebalew2020@gmail.com>
2023-05-18 12:55:50 -0400

    calculate number

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+function calculateNumber(a, b) {
+    const round_a = Math.round(a);
+    const round_b = Math.round(b);
+    return round_a + rounded_b;
+  }
+  
+  module.exports = calculateNumber;
