fcf97fd1842ebca05a9091da294386a5edd95ac6
GetieBalew24 <getiebalew2020@gmail.com>
2023-05-18 16:06:04 -0400

    update payement task 6

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+const getPaymentTokenFromAPI = (success) => {
+  if (success) {
+    return Promise.resolve({ data: 'Successful response from the API' });
+  } else {
+    return new Promise(() => {});
+  }
+};
+
+module.exports = getPaymentTokenFromAPI;
